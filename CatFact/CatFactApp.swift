7ba9c4852569065d1522fde0a5bcad5774f51cab
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-01-17 11:51:45 +1100

    Initial Commit

---
+++ CatFact/CatFactApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatFactApp.swift
+//  CatFact
+//
+//  Created by Midori Verdouw on 17/1/22.
+//
+
+import SwiftUI
+
+@main
+struct CatFactApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
