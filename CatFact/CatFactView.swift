01d31e0cc3b553c786216b60d0ce748965e1672d
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-01-18 22:03:52 +1100

    Add a view to be shown when loading

--- CatFact/CatFactView.swift
+++ CatFact/CatFactView.swift
@@ -14,7 +14,7 @@ struct CatFactView: View {
         Group {
             switch viewModel.loadState {
             case .idle, .loading:
-                ProgressView("Loading...")
+                loadingView
             case let .loaded(catFacts):
                 List(catFacts) { catFact in
                     Text(catFact.fact)
@@ -25,6 +25,18 @@ struct CatFactView: View {
         }
         .task {
             await viewModel.loadCatFacts()
+        }
+    }
+    
+    var loadingView: some View {
+        GeometryReader { geometry in
+            VStack(alignment: .center) {
+                Image("cat1")
+                    .resizable()
+                    .frame(width: geometry.size.width * 0.6, height: geometry.size.height * 0.2)
+                ProgressView("Loading...")
+            }
+            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
         }
     }
 }
