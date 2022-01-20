004e41aede051e1bc57b5dc06058f4be133e2c65
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-01-20 11:55:10 +1100

    Add a cat image shown together with the text

--- CatFact/ChangingView.swift
+++ CatFact/ChangingView.swift
@@ -9,19 +9,29 @@ import SwiftUI
 
 struct ChangingView: View {
     @EnvironmentObject private var viewModel: CatFactViewModel
-        
+    
     var body: some View {
-        VStack {
-            Text(viewModel.catFacts[viewModel.currentFactIndex].fact)
+        GeometryReader { geometry in
+            VStack {
+                Group {
+                    Image("cat2")
+                        .resizable()
+                        .scaledToFit()
+                }
+                .frame(width: geometry.size.width, height: geometry.size.height * 0.3)
+                                    
+                Text(viewModel.catFacts[viewModel.currentFactIndex].fact)
+            }
+            .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
+            .onAppear {
+                viewModel.startTimer()
+            }
+            .id(viewModel.currentFactIndex)
+            .transition(
+                AnyTransition
+                    .asymmetric(insertion: .opacity, removal: .identity)
+                    .animation(.easeInOut(duration: 1))
+            )
         }
-        .onAppear {
-            viewModel.startTimer()
-        }
-        .id(viewModel.currentFactIndex)
-        .transition(
-            AnyTransition
-                .asymmetric(insertion: .opacity, removal: .identity)
-                .animation(.easeInOut(duration: 1))
-        )
     }
 }
