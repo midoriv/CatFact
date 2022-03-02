6289343d956989972ee8f8adaced67a74660756e
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-03-02 11:34:12 +1100

    Create a new SingleMessageView to be used instead of noFavouriteView

--- CatFact/Views/FavouritesView.swift
+++ CatFact/Views/FavouritesView.swift
@@ -12,7 +12,7 @@ struct FavouritesView: View {
     
     var body: some View {
         if viewModel.favourites.isEmpty {
-            noFavouriteView
+            SingleMessageView()
         }
         else {
             List {
@@ -26,38 +26,8 @@ struct FavouritesView: View {
             .navigationTitle("Favourites")
         }
     }
-    
-    var noFavouriteView: some View {
-        GeometryReader { geometry in
-            ZStack {
-                Image("homeBackground")
-                    .resizable()
-                    .scaledToFill()
-                    .opacity(0.9)
-                    .edgesIgnoringSafeArea(.all)
-            
-                VStack(alignment: .center) {
-                    Image("cat10")
-                        .resizable()
-                        .scaledToFit()
-                    Text("No favourite yet...")
-                }
-                .frame(width: getContentWidth(in: geometry), height: getContentHeight(in: geometry))
-            }
-            .frame(width: geometry.size.width, height: geometry.size.height)
-        }
-    }
-    
-    func getContentWidth(in geometry: GeometryProxy) -> CGFloat {
-        geometry.size.width < geometry.size.height ?
-            geometry.size.width : geometry.size.width * 0.25
-    }
-    
-    func getContentHeight(in geometry: GeometryProxy) -> CGFloat {
-        geometry.size.width < geometry.size.height ?
-        geometry.size.height * 0.3 : geometry.size.height
-    }
 }
+
 
 struct FavouritesView_Previews: PreviewProvider {
     static var previews: some View {
---
+++ CatFact/Views/SingleMessageView.swift
@@ -0,0 +1,47 @@
+//
+//  SingleMessageView.swift
+//  CatFact
+//
+//  Created by Midori Verdouw on 2/3/2022.
+//
+
+import SwiftUI
+
+struct SingleMessageView: View {
+    var body: some View {
+        GeometryReader { geometry in
+            ZStack {
+                Image("homeBackground")
+                    .resizable()
+                    .scaledToFill()
+                    .opacity(0.9)
+                    .edgesIgnoringSafeArea(.all)
+            
+                VStack(alignment: .center) {
+                    Image("cat10")
+                        .resizable()
+                        .scaledToFit()
+                    Text("No favourite yet...")
+                }
+                .frame(width: getContentWidth(in: geometry), height: getContentHeight(in: geometry))
+            }
+            .frame(width: geometry.size.width, height: geometry.size.height)
+        }
+    }
+    
+    func getContentWidth(in geometry: GeometryProxy) -> CGFloat {
+        geometry.size.width < geometry.size.height ?
+            geometry.size.width : geometry.size.width * 0.25
+    }
+    
+    func getContentHeight(in geometry: GeometryProxy) -> CGFloat {
+        geometry.size.width < geometry.size.height ?
+        geometry.size.height * 0.3 : geometry.size.height
+    }
+}
+
+struct SingleMessageView_Previews: PreviewProvider {
+    static var previews: some View {
+        SingleMessageView()
+    }
+}
