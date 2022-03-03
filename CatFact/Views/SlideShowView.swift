71e6cb73502c6a46b73cf7c082add29b2ca99672
Midori Verdouw <s3575912@student.rmit.edu.au>
2022-03-03 11:24:59 +1100

    Refactor SlideShowView using layBackground view modifier

--- CatFact/Views/SlideShowView.swift
+++ CatFact/Views/SlideShowView.swift
@@ -11,50 +11,33 @@ struct SlideShowView: View {
     @EnvironmentObject private var viewModel: CatFactViewModel
     
     var body: some View {
-        ZStack {
-            Image("homeBackground")
-                .resizable()
-                .scaledToFill()
-                .opacity(0.9)
-                .edgesIgnoringSafeArea(.all)
-        
-            Group {
-                // if favourite item does not exist
-                if viewModel.favourites.isEmpty {
-                    SingleMessageView(message: "No favourite yet...")
-                }
-                else {
-                    VStack(spacing: 100) {
-                        NavigationLink(destination: ChangingView().environmentObject(viewModel)) {
-                            Text("View slide show of your favourite cat facts")
-                        }
-                        .simultaneousGesture(TapGesture().onEnded {
-                            viewModel.setSlideShowMode(.favourite)
-                        })
-                        
-                        NavigationLink(destination: ChangingView().environmentObject(viewModel)) {
-                            Text("View slide show of random cat facts")
-                        }
-                        .simultaneousGesture(TapGesture().onEnded {
-                            viewModel.setSlideShowMode(.random)
-                        })
-                    }
-                }
+        Group {
+            // if favourite item does not exist
+            if viewModel.favourites.isEmpty {
+                SingleMessageView(message: "No favourite yet...")
             }
-            .onAppear {
-                viewModel.setCurrentFactIndex(0)
+            else {
+                VStack(spacing: 100) {
+                    NavigationLink(destination: ChangingView().environmentObject(viewModel)) {
+                        Text("View slide show of your favourite cat facts")
+                    }
+                    .simultaneousGesture(TapGesture().onEnded {
+                        viewModel.setSlideShowMode(.favourite)
+                    })
+                    
+                    NavigationLink(destination: ChangingView().environmentObject(viewModel)) {
+                        Text("View slide show of random cat facts")
+                    }
+                    .simultaneousGesture(TapGesture().onEnded {
+                        viewModel.setSlideShowMode(.random)
+                    })
+                }
             }
         }
-    }
-    
-    func getContentWidth(in geometry: GeometryProxy) -> CGFloat {
-        geometry.size.width < geometry.size.height ?
-            geometry.size.width : geometry.size.width * 0.3
-    }
-    
-    func getContentHeight(in geometry: GeometryProxy) -> CGFloat {
-        geometry.size.width < geometry.size.height ?
-        geometry.size.height * 0.3 : geometry.size.height * 0.5
+        .layBackground()
+        .onAppear {
+            viewModel.setCurrentFactIndex(0)
+        }
     }
 }
 
